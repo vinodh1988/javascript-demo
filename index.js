0824f12eaaf7624cd1ffb9fc61ebe6f154f65deb
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-22 14:57:59 +0530

    first-commt

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("number of h1",document.getElementsByTagName("h1").length);
+
+headers=document.getElementsByTagName("h1")
+
+headers[0].style.fontFamily="Arial";
+headers[1].style.color="green";
+headers[2].style.backgroundColor="yellow";
