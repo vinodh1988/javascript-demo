b4fdd5cd84ed65bf944659d8e610760aae061606
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-22 15:21:22 +0530

    first

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ headers=document.getElementsByTagName("h1")
 headers[0].style.fontFamily="Arial";
 headers[1].style.color="green";
 headers[2].style.backgroundColor="yellow";
+
+function apply(){
+   let lc= document.getElementById("listcolor").value;
+   let lf= document.getElementById("listfont").value;
+   let c= document.getElementById("continent").value;
+   document.getElementsByTagName("li")[c].style.color=lc;
+   document.getElementsByTagName("li")[c].style.fontFamily=lf;
+}
