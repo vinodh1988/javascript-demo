0e4150bc5818b9753aaf3e86d23ea5a37f0ecdad
vinodh1988 <k.c.vinodh@hotmail.com>
2022-06-22 17:14:30 +0530

    table

--- store.js
+++ store.js
@@ -1,10 +1,17 @@
-store=[]
+//store=[]
 
+code="";
 function add(){
     let sno=document.forms[0].sno.value;
     let name=document.forms[0].name.value;
     let city=document.forms[0].city.value;
     let person={sno:sno,name:name,city:city}
-    store.push(person);
-    console.log(store)
+    code+="<tr>";
+    code+="<td>"+sno+"</td>";
+    code+="<td>"+name+"</td>";
+    code+="<td>"+city+"</td>";
+    code+="</tr>";
+
+    console.log(code);
+    document.getElementById("tdata").innerHTML = code;
 }
